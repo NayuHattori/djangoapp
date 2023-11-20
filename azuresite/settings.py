e9d2abd114de947865b65621ed733c23bbec5fe5
NayuHattori <140383457+NayuHattori@users.noreply.github.com>
2023-11-20 18:47:45 +0900

    Update settings.py

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'testdjangoapp20231120'
+SECRET_KEY = '8c4l68n8i2&$j37%^gn9spur682b+kfp22&&^qnodge8462cg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
